1500ae5de9980905b62c2cb6f28c685972ad4f06
Song Gao <song@gao.io>
2013-04-21 20:07:42 -0500

    init.sh: install squirrel, olsrd, iperf, etc.;

--- build_all.sh
+++ build_all.sh
@@ -6,4 +6,11 @@ DIR=$(cd $(dirname $0) && pwd)
 export VANILLA='test-vanilla'
 export VANILLA_INIT=$DIR/init.sh
 
-lxc_scripts/create.sh && lxc_scripts/clone.sh
+cd files
+python -m SimpleHTTPServer 8099 &
+SERVER_PID=$!
+cd $DIR
+
+lxc_scripts/create.sh #&& lxc_scripts/clone.sh
+echo $SERVER_PID
+kill $SERVER_PID
--- init.sh
+++ init.sh
@@ -1,4 +1,27 @@
 #!/bin/bash
 
-echo "Hello!" > /home/ubuntu/haha
-chmod a+r /home/ubuntu/haha
+apt-get update
+apt-get -y install curl wget iperf
+
+# make sure /dev/net/tun is available
+mkdir -p /dev/net
+mknod /dev/net/tun c 10 200
+chmod 600 /dev/net/tun
+
+# install squirrel
+cd /usr/local/bin
+wget http://10.0.3.1:8099/squirrel
+chmod a+rx squirrel
+echo '{
+    "ServerAddress":    "10.0.3.1:1234",
+    "TapInterfaceName": "i.am.not.real",
+    "Identity":         '$(number)'
+}' > /etc/squirrel.client.conf
+
+# install olsrd
+apt-get -y install bison flex make gcc
+mkdir -p /tmp/olsrd_tmp
+cd /tmp/olsrd_tmp
+wget http://www.olsr.org/releases/0.6/olsrd-0.6.5.2.tar.gz && tar zxvf olsrd-0.6.5.2.tar.gz
+cd olsrd-0.6.5.2 && make && make install
+curl http://10.0.3.1:8099/olsrd.conf > /etc/olsrd.conf
